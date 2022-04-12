13eee0c3445c1cbe8f497e4c739b9d27f3791217
Trevor <tkoenig@udel.edu>
2022-04-12 15:03:25 -0400

    skeleton of semesterBuilder

---
+++ src/semesterBuilder.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Button, Row, Col, Container } from "react-bootstrap";
+
+interface ScheduleProps {
+    setSchedule: (newSchedule: ) => void;
+    colorIndex: number;
+}
+
+export function ScheduleBuilder(): JSX.Element {
+    return <div></div>;
+}
+
+function generateSchedule(): {
+
+};
+
+
+function addClass(): void {
+    return;
+}
+
