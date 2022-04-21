e82b8511a90a1fd4737435f6b062bc3d21b0b57c
Trevor <tkoenig@udel.edu>
2022-04-21 14:07:31 -0400

    goofed around and improved semesterBuilder

--- src/semesterBuilder.tsx
+++ src/semesterBuilder.tsx
@@ -1,17 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form, Button, Row, Col, Container } from "react-bootstrap";
 import { Course } from "./interfaces/course";
+
+import allCourses from `../catalog.json`
 
 interface semesterProps {
     setSemester: (newSemester: Course[]) => void;
     semester: Course[];
 }
 
+interface courseProps {
+    setCourse: (newCourse: Course) => void;
+    course: Course;
+}
+
+function getCourseByID(id: string): Course {
+    let flag: string = id.map(c => {
+        parseInt(c)
+    });
+    
+    
+}
+
 export function semesterBuilder(): JSX.Element {
     const [tempCourseName, setTempCourseName] = useState<string>("");
     return (
         <div>
-            <Form.Group controlId="name-box">
+            <Form.Group controlId="name-box" onClick="generate">
                 <Form.Label>Joe Shmoe</Form.Label>
                 <Form.Control value={userName} onChange={editName} />
                 <span></span>
