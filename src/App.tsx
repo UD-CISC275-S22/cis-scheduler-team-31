8a6934ddca63dd1dee20252138b5d6683953074f
trevorkoenig <tkoenig@udel.edu>
2022-05-14 18:10:37 -0400

    messing with simple course builder

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
 import React from "react";
 import "./App.css";
+import "./courseMaker";
+import { InputCrseInfo } from "./courseMaker";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">Scheduling Helper</header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <InputCrseInfo></InputCrseInfo>
         </div>
     );
 }
